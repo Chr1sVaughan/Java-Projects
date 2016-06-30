0c794bf7c051b92237edcb5be83a2c0e0ca21766
Chr1sVaughan <sojellysickle@gmail.com>
2016-06-29 17:08:19 -0700

    java template files
    Signed-off-by: Chr1sVaughan <sojellysickle@gmail.com>

---
+++ Template.java
@@ -0,0 +1,18 @@
+// Chris Vaughan
+// IT145
+// June 16th 2016
+// Week 1 Homework
+
+
+
+
+
+public class Template
+{
+   public static void main(String[] args)
+   {
+      
+      System.out.println("Welcome to Java IT145");
+      
+   }
+}
